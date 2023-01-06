1fd0010b98885fec3a380bc4ed020004e974c22a
Chisom Chukwuma <chukwumachisom18@gmail.com>
2023-01-06 04:15:29 -1100

    vite + react app

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+import './style/Form.css'
+
+function App() {
+
+  return (
+    <div className="App">
+      <h4>hello</h4>
+    </div>
+  )
+}
+
+export default App
